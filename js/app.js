9f178a64855e043f6deba29e5af9ac02be38584c
Jaminur islam <jaminurislam250@gmail.com>
2021-09-02 09:54:12 +0600

    something add

--- js/app.js
+++ js/app.js
@@ -1,9 +1,8 @@
-// No result found function [multi time use]
+//  result found function [multi time using]
 const ResultFound = (id, property) => {
   const noResult = document.getElementById(id)
   noResult.style.display = property;
 }
-
 // button add Event listener
 document.getElementById('button').addEventListener('click', async () => {
   // capture input value
@@ -34,27 +33,25 @@ document.getElementById('button').addEventListener('click', async () => {
     const url = `https://openlibrary.org/search.json?q=${inputText}`;
     const res = await fetch(url);
     const data = await res.json();
-    // total founded result capture
-    const totalFound = data.numFound;
 
-    // set total books found result
-    const totaltotalFound = document.getElementById('total');
-    totaltotalFound.innerText = totalFound;
+  // set total books found result
+    const totalLen = document.getElementById('total');
+    totalLen.innerText = data.numFound;
 
     // Call function
     displayInformation(data.docs)
 
   } catch (er) {
+    console.log(er)
     ResultFound('result', 'block');
     // remove spiners
     ResultFound('spiners', 'none')
   }
 })
-
 // displayInformation function
 const displayInformation = (data) => {
 
-  if (data.totalFoundgth === 0) {
+  if (data.length === 0) {
     //add  No result found Message
     ResultFound('result', 'block');
     // remove spiners
@@ -64,8 +61,8 @@ const displayInformation = (data) => {
     // remove No result found Message
     ResultFound('result', 'none')
   }
-  // get total Array totalFoundgth
-  const totalFound = data.totalFoundgth;
+  // get total Array length
+  const len = data.length;
 
   // show total search value
   ResultFound('total-search-value', 'block')
@@ -86,9 +83,9 @@ const displayInformation = (data) => {
       <img src="https://covers.openlibrary.org/b/id/${singleData.cover_i ? singleData.cover_i : 'No picture founded'}-M.jpg" class="card-img-top" alt="Book image" style = "width: 150px; height: 200px;">
       </div>
       <div class="card-body">
-        <h5 class="card-title">Book Name: ${singleData.title}</h5>
-        <h5 class="card-title"> Writer : ${singleData.author_name ? singleData.author_name[0]: 'Unknown author'}</h5>
-        <p class="card-text"> First Publish Data ${singleData.first_publish_year ? singleData.first_publish_year: 'Unknown Year'}</p>
+        <h5 class="card-title">BookName: ${singleData.title}</h5>
+        <h5 class="card-title"> lekhok : ${singleData.author_name ? singleData.author_name[0]: 'Unknown author'}</h5>
+        <p class="card-text"> firstPublishData ${singleData.first_publish_year ? singleData.first_publish_year: 'Unknown Year'}</p>
       </div>
     </div>`
     displayContainer.appendChild(div);
